8840c587e01054d8735aafb104f70e71bb7ca80d
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2021-04-25 15:12:10 +0300

    Load textdomain

--- functions.php
+++ functions.php
@@ -8,6 +8,17 @@
  */
 
 /**
+ * Set up child theme's textdomain.
+ *
+ * Declare textdomain for this child theme.
+ * Translations can be added to the /languages/ directory.
+ */
+function twentytwentyonechild_theme_setup() {
+	load_child_theme_textdomain( 'twentytwentyonechild', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'twentytwentyonechild_theme_setup' );
+
+/**
  * Enqueue scripts and styles.
  *
  * @return void
